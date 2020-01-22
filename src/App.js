447176971641b8e426e8e02393c665b48ca6f7dd
sunyamare <sunyamare@gmail.com>
2020-01-22 11:48:00 +0100

    added header and footer to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>header</h1>
+      <Header />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div class="site-notice">
+        <span>
+          Site notice: This is a demo page! Contact:
+          <a href="https://sunyamare.ch/" target="_blank" rel="noopener">
+            sunyamare.ch
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+}
