5a05514dd794df5aa6cd6bbbaecef880f0490f28
sunyamare <sunyamare@gmail.com>
2020-01-24 16:20:58 +0100

    added arrow lightbox navigation

--- src/components/Books.js
+++ src/components/Books.js
@@ -4,6 +4,24 @@ import Book from "./Book";
 import "./Books.css";
 
 export class Books extends Component {
+  componentDidMount() {
+    document.addEventListener("keydown", event => {
+      if (event.keyCode === 27) {
+        window.location.replace("#!");
+      }
+      if (event.keyCode === 37) {
+        window.location.replace(
+          "#img" + (parseInt(window.location.hash.substring(4)) - 1)
+        );
+      }
+      if (event.keyCode === 39) {
+        window.location.replace(
+          "#img" + (parseInt(window.location.hash.substring(4)) + 1)
+        );
+      }
+    });
+  }
+
   render() {
     return (
       <div id="contentSection" className="container">
