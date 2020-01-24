b33fe74f9a54997f020465169cf91e8e745838c6
sunyamare <sunyamare@gmail.com>
2020-01-24 11:47:06 +0100

    moved axios to App

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,31 +1,13 @@
 import React, { Component } from "react";
 import Book from "./Book";
 
-import axios from "axios";
-
 import "./Books.css";
 
 export class Books extends Component {
-  state = {
-    books: []
-  };
-
-  componentDidMount() {
-    axios
-      .get("https://api.myjson.com/bins/zyv02")
-      .then(response =>
-        response.data.books.map((book, index) => {
-          book.id = index;
-          return book;
-        })
-      )
-      .then(response => this.setState({ books: response }));
-  }
-
   render() {
     return (
       <div id="contentSection" className="container">
-        {this.state.books.map((book, index) => (
+        {this.props.books.map((book, index) => (
           <Book key={index} book={book} />
         ))}
       </div>
